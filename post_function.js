c036fac4e4cae107b76f1a716d785bd94ea4b40b
victorma17 <victorma17@users.noreply.github.com>
2023-10-03 11:16:02 +0200

    update

--- post_function.js
+++ post_function.js
@@ -2,10 +2,16 @@ const ex = require("express")
 
 const app = ex()
 
+app.use(ex.static("public", {
+    index: "p1.html"
+}))
 app.use(ex.json())
 app.use(ex.urlencoded())
 
 app.listen(8088, '127.0.0.1')
+
+
+app.get("/")
 
 app.post("/echoPost/:cliente/:factura", (request, response) => {
     response.send({
@@ -15,4 +21,11 @@ app.post("/echoPost/:cliente/:factura", (request, response) => {
     })
 })
 
+app.post("/echopost/:cliente/:factura", (request, response) => {
+    response.send({
+        body:request.body,
+        query: request.query,
+        params: request.params
+    })
+})
 
